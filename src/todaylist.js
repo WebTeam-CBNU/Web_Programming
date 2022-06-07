b63db4ec50210aab50725609468435f429811d8a
Jeong jaemin <jmjung1997@gmail.com>
2022-06-07 19:26:33 +0900

    todaylist

--- src/todaylist.js
+++ src/todaylist.js
@@ -1,63 +1,54 @@
-// getting all required elements
-const inputBox = document.querySelector(".inputField input");
-const addBtn = document.querySelector(".inputField button");
-const todoList = document.querySelector(".todoList");
-const deleteAllBtn = document.querySelector(".footer button");
-// onkeyup event
-inputBox.onkeyup = ()=>{
-  let userEnteredValue = inputBox.value; //getting user entered value
-  if(userEnteredValue.trim() != 0){ //if the user value isn't only spaces
-    addBtn.classList.add("active"); //active the add button
-  }else{
-    addBtn.classList.remove("active"); //unactive the add button
-  }
+
+
+
+function hello()  {
+  alert('안녕하세요');
 }
-showTasks(); //calling showTask function
-addBtn.onclick = ()=>{ //when user click on plus icon button
-  let userEnteredValue = inputBox.value; //getting input field value
-  let getLocalStorageData = localStorage.getItem("New Todo"); //getting localstorage
-  if(getLocalStorageData == null){ //if localstorage has no data
-    listArray = []; //create a blank array
-  }else{
-    listArray = JSON.parse(getLocalStorageData);  //transforming json string into a js object
+
+function makeTable() {
+  var computer_id = 1;
+  td_html = "<input type='checkbox' name='radios[]'  class='radios' id='radio" + computer_id + "'>";
+
+  const weekly_date = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT','비고']
+
+
+  for (var i = 0; i < 3; i++) {
+      var table = document.createElement("table");
+      document.getElementsByClassName("seat-table")[i].appendChild(table);
+      for (var j = 0; j < 4; j++) {
+          var tr = document.createElement("tr");
+          table.appendChild(tr);
+
+          var td1 = document.createElement("td");
+          td1.innerHTML = "<input type='checkbox' name='radios[]' value='123' class='radios' id='radio" + computer_id + "'><label for='radio" + computer_id + "'> " + `${weekly_date[j]}</label>`; //td_html
+          computer_id += 1;
+
+          var td2 = document.createElement("td");
+          td2.innerHTML = "<input type='checkbox' name='radios[]' value='123' class='radios' id='radio" + computer_id + "'><label for='radio" + computer_id + "'> " + `${weekly_date[j+4]}</label>`; //td_html
+          computer_id += 1;
+
+          tr.appendChild(td1);
+          tr.appendChild(td2);
+      }
   }
-  listArray.push(userEnteredValue); //pushing or adding new value in array
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); //transforming js object into a json string
-  showTasks(); //calling showTask function
-  addBtn.classList.remove("active"); //unactive the add button once the task added
-}
-function showTasks(){
-  let getLocalStorageData = localStorage.getItem("New Todo");
-  if(getLocalStorageData == null){
-    listArray = [];
-  }else{
-    listArray = JSON.parse(getLocalStorageData); 
-  }
-  const pendingTasksNumb = document.querySelector(".pendingTasks");
-  pendingTasksNumb.textContent = listArray.length; //passing the array length in pendingtask
-  if(listArray.length > 0){ //if array length is greater than 0
-    deleteAllBtn.classList.add("active"); //active the delete button
-  }else{
-    deleteAllBtn.classList.remove("active"); //unactive the delete button
-  }
-  let newLiTag = "";
-  listArray.forEach((element, index) => {
-    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-  });
-  todoList.innerHTML = newLiTag; //adding new li tag inside ul tag
-  inputBox.value = ""; //once task added leave the input field blank
-}
-// delete task function
-function deleteTask(index){
-  let getLocalStorageData = localStorage.getItem("New Todo");
-  listArray = JSON.parse(getLocalStorageData);
-  listArray.splice(index, 1); //delete or remove the li
-  localStorage.setItem("New Todo", JSON.stringify(listArray));
-  showTasks(); //call the showTasks function
-}
-// delete all tasks function
-deleteAllBtn.onclick = ()=>{
-  listArray = []; //empty the array
-  localStorage.setItem("New Todo", JSON.stringify(listArray)); //set the item in localstorage
-  showTasks(); //call the showTasks function
+
+  // 예약하기 버튼에 이벤트넣기
+  let book_btn = document.getElementById("book");
+  book_btn.addEventListener('click', function(){
+      let checked=[];
+      let checked_message="";
+      let radios=document.querySelectorAll(".radios");
+      for(let i=0; i<radios.length; i++){
+          if(radios[i].checked){
+              checked.push(radios[i]);
+              checked_message += (i+1)+" ";
+          }
+      }
+      //선택된 거 있나 없나 검사
+      if(checked.length!=0){  //뭐라도 선택했다
+          alert(checked_message+"선택됨")
+      } else{
+          alert("아무것도 없음");
+      }
+  })
 }
