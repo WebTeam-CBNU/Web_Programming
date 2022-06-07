b1fd83d666c87b82485a2208e51bbd0919a61166
JeonghyeonK <6843wjdgus@naver.com>
2022-06-07 15:08:21 +0900

    이거아님

--- src/main.js
+++ src/main.js
@@ -48,13 +48,13 @@ const c = document.getElementById("clock");
 const dpr = window.devicePixelRatio;  
 
 const ctx = c.getContext('2d');
-c.width =  c.width * dpr;
-c.height = c.height * dpr;
+c.width =  c.width * dpr*2;
+c.height = c.height * dpr*2;
 ctx.scale(dpr, dpr);
 // const clockCenterX = c.width / 2;
 // const clockCenterY = c.height / 2;
-let radius = c.height / 4;
-ctx.translate(radius*2, radius);
+let radius = c.height / 3.5;
+ctx.translate(radius*2.7, radius*1.35);
 radius = radius * 0.90
 setInterval(drawClock, 1000);
 
