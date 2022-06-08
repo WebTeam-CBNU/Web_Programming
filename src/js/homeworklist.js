212d8ce13cad3380cafc928091a4d71a5ff18ced
JeonghyeonK <6843wjdgus@naver.com>
2022-06-08 22:21:46 +0900

    살려줘

---
+++ src/js/homeworklist.js
@@ -0,0 +1,23 @@
+
+
+//체크리스트 만드는 함수
+function createHomework() {
+    const newHomework = document.createElement('div'); 
+
+    const subjectArray = ["subject1, subject2"];
+
+    // test 줄입니다
+    let testElement = Array.from(document.querySelectorAll(`homework`))
+    let fuck = document.getElementsByClassName("add-homework")[0]
+    fuck.innerHTML = testElement.length
+
+    newHomework.setAttribute("class", "homework");
+    // 1. 몇번째 child인지 알 수 있는 방법 찾기
+    // 2. 몇번째 차일드인지 저장하는 변수 구현
+    // 3. 그 변수를 밑에 subjectArray에 적용
+
+    newHomework.innerHTML =
+          `<input type='checkbox' name='${subjectArray[0]}' value='homework1' style='margin:6px;'/><p>새로운 과제~</p>`;
+    // 4. <body>에 1에서 만든 <div> element 붙이기
+    document.getElementById("subject1").appendChild(newHomework);
+  } 
