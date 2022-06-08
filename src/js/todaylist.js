f9f7da64caeede82b4c61e800ff946948b7bcd2d
patrache <ssk5442@naver.com>
2022-06-09 02:51:29 +0900

    sunday btn added

--- src/js/todaylist.js
+++ src/js/todaylist.js
@@ -5,19 +5,21 @@ function addCheck() {
     checklist.innerHTML = "fuck!"
 }
 
-const tempArray = ["test1", "test3", "test5"]
+const tempArray = ["test12", "test11", "test10", "test9", "test8", "test7", "test6", "test5", "test4", "test3", "test2", "test1"]
 
 for (let i = 0; i < tempArray.length; i++) {
-    document.getElementById(tempArray[i]).innerHTML=tempArray[i];
+    document.querySelectorAll("#amBox div")[i].innerHTML=tempArray[i];
 }  
 
 function showSchedule()
 {
-    const con = document.getElementsByClassName('pmBox');
-    Array.from(con).forEach((element, index) => {
+    const pmBox = document.getElementsByClassName('pmBox');
+    const amBox = document.getElementsByClassName('amBox');
+    const allBox = Array.from(amBox).concat(Array.from(pmBox));
+    Array.from(allBox).forEach((element) => {
         if(true){
             console.log(element)
-            // element.style.visibility = 'visible';
+            element.style.visibility = 'visible';
             // for(var i=1; i<=12; i++){
             //     if(document.getElementById("1a").innerHTML !== "")
             //         document.getElementById("1a").innerHTML = "you 01a do"
