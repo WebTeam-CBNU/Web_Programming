42869fe776056913aebd2b7d94e2a773a4aba923
patrache <ssk5442@naver.com>
2022-06-08 21:23:24 +0900

    merge for pull

---
+++ src/js/todaylist.js
@@ -0,0 +1,7 @@
+
+let plsBtn = document.getElementsByClassName("plsBtn")[0];
+let checklist = document.getElementsByClassName("checklist")[0];
+
+function addCheck() {
+    checklist.innerHTML = "fuck!"
+}
