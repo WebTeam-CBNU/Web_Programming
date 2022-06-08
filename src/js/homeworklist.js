fe50b0b3efd9e7aadb9b2079720c49ba28d41759
JeonghyeonK <6843wjdgus@naver.com>
2022-06-09 06:43:09 +0900

    이것저것:

--- src/js/homeworklist.js
+++ src/js/homeworklist.js
@@ -1,23 +1,45 @@
 
 
 //체크리스트 만드는 함수
-function createHomework() {
+function createHomework(subjectId) {
     const newHomework = document.createElement('div'); 
 
-    const subjectArray = ["subject1, subject2"];
-
-    // test 줄입니다
-    let testElement = document.querySelectorAll(`#subject1 .homework`)
+    let homeworkIndex = document.querySelectorAll(`#${subjectId} .homework`)
     let fuck = document.getElementsByClassName("add-homework")[0]
-    fuck.innerHTML = testElement.length
+    const homeworkNum='homework'+(homeworkIndex.length+1)
+    fuck.innerHTML = homeworkNum
 
     newHomework.setAttribute("class", "homework");
-    // 1. 몇번째 child인지 알 수 있는 방법 찾기
-    // 2. 몇번째 차일드인지 저장하는 변수 구현
-    // 3. 그 변수를 밑에 subjectArray에 적용
+    
+    newHomework.innerHTML = 
+          `<input type='checkbox' name='${subjectId}' value='${homeworkNum}' style='margin:6px;'/><p>새로운 과제~</p>`;
 
-    newHomework.innerHTML =
-          `<input type='checkbox' name='${subjectArray[0]}' value='homework1' style='margin:6px;'/><p>새로운 과제~</p>`;
-    // 4. <body>에 1에서 만든 <div> element 붙이기
-    document.getElementById("subject1").appendChild(newHomework);
+    document.getElementById(`${subjectId}`).appendChild(newHomework);
+  } 
+
+  function createSubject() {
+
+    const deletedSubject = document.getElementById('subject-plus');
+    deletedSubject.remove();
+
+
+    const newSubject = document.createElement('div'); 
+
+    let SubjectIndex = document.querySelectorAll(`#subject-list .subject-block`)
+    const subjectNum='subject'+(SubjectIndex.length+1)
+
+    newSubject.setAttribute("class", "subject-block");
+    newSubject.innerHTML = `
+    <div class="subject-title">과목 이름</div>
+    <div id='${subjectNum}'>
+    </div>
+    <div class="add-homework" onclick="createHomework('${subjectNum}')">+</div>`;          
+    document.getElementById("subject-list").appendChild(newSubject);
+
+
+    const createBlock = document.createElement('div'); 
+    createBlock.setAttribute("id", "subject-plus");
+    createBlock.setAttribute("class", "subject-block");
+    createBlock.innerHTML = `<div class='subject-plus' onclick='createSubject()'>+</div>`;          
+    document.getElementById("subject-list").appendChild(createBlock);
   } 
