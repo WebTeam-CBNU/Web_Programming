49eda0b5049547fd197efd07ce6bddf113f5d794
Jeong jaemin <jmjung1997@gmail.com>
2022-06-09 02:32:04 +0900

    sngsu

--- src/js/todaylist.js
+++ src/js/todaylist.js
@@ -1,7 +1,32 @@
-
 let plsBtn = document.getElementsByClassName("plsBtn")[0];
 let checklist = document.getElementsByClassName("checklist")[0];
 
 function addCheck() {
     checklist.innerHTML = "fuck!"
+}
+
+const tempArray = ["test1", "test3", "test5"]
+
+for (let i = 0; i < tempArray.length; i++) {
+    document.getElementById(tempArray[i]).innerHTML=tempArray[i];
+}  
+
+function showSchedule()
+{
+    const con = document.getElementsByClassName('pmBox');
+    Array.from(con).forEach((element, index) => {
+        if(true){
+            console.log(element)
+            // element.style.visibility = 'visible';
+            // for(var i=1; i<=12; i++){
+            //     if(document.getElementById("1a").innerHTML !== "")
+            //         document.getElementById("1a").innerHTML = "you 01a do"
+            //     else
+            //         console.log("test")
+            // }
+            
+        }else{
+            con.style.visibility = 'hidden';
+        }
+    });
 }
